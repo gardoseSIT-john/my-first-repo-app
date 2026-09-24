090608b0b1f4759d1e53127a19eb5f320e8aafc1
gardoseSIT-john <20187151@s.ubaguio.edu>
2026-09-24 08:51:50 +0800

    feat: updated app title with github greeting

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,5 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   //protected readonly title = signal('my-first-repo-app');
   title = 'Hello Github, this is my first Angular Push!'
+  testTitle = "JEWOO"
 }
